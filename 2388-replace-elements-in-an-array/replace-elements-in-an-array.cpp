b0f6f7916628c7923edfda4e78f561f893edf51a
Abhay Mishra <abhaym1205@gmail.com>
2024-06-28 15:49:22 +0530

    Time: 404 ms (22.56%) | Memory: 143.2 MB (46.13%) - LeetSync

--- 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
 
 
     void solve(vector<int>& nums,vector<vector<int>>& operations){
-        // now we are getting tle so we use map toi find the element the data in O(N)
+        // now we are getting tle so we use map to find the element the data in O(N)
         map<int,int> mp;
         for(int i=0; i<nums.size(); i++){
             mp[nums[i]]=i;
@@ -18,8 +18,7 @@ public:
                 mp.erase(it);
                 // adding new value
                 mp[operations[i][1]]=f;
-                // cout<<it->first<<" : "<<it->second;
-                // nums[f]= operations[i][1];
+
             }
             
         }
