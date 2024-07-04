581e36e2caa29cfa5d94c213df71a9c288cf08f2
Abhay Mishra <abhaym1205@gmail.com>
2024-07-04 22:28:33 +0530

    Time: 47 ms (5.16%) | Memory: 40.3 MB (6.12%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        list<int> ls;
+        for(auto n:nums){
+            ls.push_back(n);
+        }
+        // applying the k operation 
+        for(int i=0; i<k; i++){
+            // 1st get last value and then remove it and then add it front
+            int lastVal = ls.back();
+            // remove the last value 
+            ls.pop_back();
+
+            // now add the last val in front
+            ls.push_front(lastVal);
+        }
+        vector<int> rotated;
+        for(auto l:ls){
+            rotated.push_back(l);
+        }
+        nums = rotated;
+    }
+};
