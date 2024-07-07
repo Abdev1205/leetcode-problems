520cfec77c6795af6f73213409d42b9e32f1b4ae
Abhay Mishra <abhaym1205@gmail.com>
2024-07-07 16:20:21 +0530

    Time: 63 ms (96.15%) | Memory: 70.1 MB (92.55%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        // this is kadane algo 
+        // where where are taking the sum whose sum is greater than the 0
+        // we are not taking sum which is lesser than 0 
+        // beacuse it will reduce our sum 
         int maxy = INT_MIN;
         int sum = 0;
         for(auto n:nums){
+            // sum ko update kar do
             sum+=n;
+            // check for maximum 
             maxy = max(maxy,sum);
+            // agar sum 0 se kam hai to use lene ka matlab nahi hai 
+            // isliye sum ko 0 save kar lete hai
             if(sum<0){
                 sum=0;
             }
