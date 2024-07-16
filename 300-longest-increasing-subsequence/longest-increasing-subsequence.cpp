67695ff11be3667d99a8ff9cf97d4735cbafd674
Abhay Mishra <abhaym1205@gmail.com>
2024-07-16 23:34:17 +0530

    Time: 243 ms (34.20%) | Memory: 294.9 MB (7.44%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -46,15 +46,37 @@ public:
 
         return dp[curr][prev+1];
     }
+
+    int solvingUsingTab(vector<int>& arr){
+        int n = arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int curr = n-1; curr>=0; curr--){
+            for(int prev=curr-1; prev>=-1; prev--){
+                // include
+                int include = 0;
+                if(prev==-1 || arr[curr] > arr[prev]){
+                    include = 1 + dp[curr+1][curr+1];
+                }
+
+                // exclude 
+                int exclude = 0 + dp[curr+1][prev+1];
+
+                int ans = max(include,exclude);
+                dp[curr][prev+1]=ans;
+                }
+        }
+        return dp[0][0];
+    }
  
     int lengthOfLIS(vector<int>& nums) {
         int curr = 0;
         int prev = -1;
         int n = nums.size();
         
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
 
         // return solveUsingRecursion(nums,curr,prev);
-        return solveUsingMemo(nums,curr,prev,dp);
+        // return solveUsingMemo(nums,curr,prev,dp);
+        return solvingUsingTab(nums);
     }
 };
