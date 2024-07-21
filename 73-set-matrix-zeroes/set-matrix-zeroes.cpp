663ba0df24d0c60b99ff65df7cdcd5b337d26ad2
Abhay Mishra <abhaym1205@gmail.com>
2024-07-21 16:21:59 +0530

    Time: 61 ms (5.30%) | Memory: 16.9 MB (10.23%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+     // trying inplace 
+     // if we will update all the value inTime so it may give wrong answer
+     // So I will create a vector of pair 
+     // we will be storing thr row and col were we have to do opeartion
+     // instead of thinking about the data consitency
+
+     vector<pair<int,int>> op;
+     // getting all the operation from the array
+     for(int row=0; row<matrix.size(); row++){
+        for(int col=0; col<matrix[row].size(); col++){
+            // if it is zero then store in vector
+            if(matrix[row][col]==0){
+                pair<int,int> p;
+                p.first = row;
+                p.second = col;
+                op.push_back(p);
+            }
+        }
+     }
+    //  for(auto o:op){
+    //     cout<<o.first<<" : "<<o.second<<endl;
+    //  }
+     // now operation start
+     for(auto o:op){
+        int row = o.first;
+        int col = o.second;
+        cout<<row<<" : "<<col<<endl;
+
+        // row operation
+        for(int i=0; i<matrix[row].size(); i++){
+            cout<<"I am updating row where "<<row<<" : "<<col<<" and value = "<<matrix[row][i]<<endl;
+            matrix[row][i]=0;
+        }
+
+        // column opearation
+        for(int j=0; j<matrix.size(); j++){
+            cout<<"I am updating col where "<<row<<" : "<<col<<" and value = "<<matrix[j][col]<<endl;
+            matrix[j][col]=0;
+        }
+     }
+
+    }
+};
