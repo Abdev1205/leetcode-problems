606cba382cbd4c3b00c140d60d9e6ec53ce7cbe9
Abhay Mishra <abhaym1205@gmail.com>
2024-07-22 23:30:43 +0530

    Time: 92 ms (27.51%) | Memory: 97.8 MB (12.28%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.cpp
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.cpp
@@ -9,9 +9,21 @@ public:
             return -1;
         }
 
-        // Approach 1 
+        // Approach 1 o(n logn)
         // using stl sort to get max and min val return in bertween of nu,mebr
-        sort(nums.begin(),nums.end());
-        return nums[1];
+        // sort(nums.begin(),nums.end());
+        // return nums[1];
+
+        // Aprroach 2 o(n)
+        int maxy = *max_element(nums.begin(),nums.end());
+        int miny = *min_element(nums.begin(),nums.end());
+
+        // now we are checkinng if no is not the min or max so we will return that
+        for(auto n:nums){
+            if(n!=maxy && n!=miny){
+                return n;
+            }
+        }
+        return -1;
     }
 };
