16b0f51a00bc6d5f8515095e97e8695cd62fc66c
Abhay Mishra <abhaym1205@gmail.com>
2024-08-03 10:42:28 +0530

    Time: 9 ms (63.29%) | Memory: 17.5 MB (57.40%) - LeetSync

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        // brute force and chapri approach 
+        // we can sort both of them and if they are equal then true else false
+
+        int n = target.size();
+        int m = arr.size();
+
+        if(n!=m){
+            return false;
+        }
+
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+
+        return target==arr;
+    }
+};
