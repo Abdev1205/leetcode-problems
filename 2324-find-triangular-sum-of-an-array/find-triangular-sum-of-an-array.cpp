03e4c0bd8a0e3f6dc43d5a3536f8764d57b20476
Abhay Mishra <abhaym1205@gmail.com>
2024-11-23 12:04:02 +0530

    Time: 360 ms (21.76%) | Memory: 382 MB (9.03%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -3,14 +3,12 @@ public:
     int triangularSum(vector<int>& nums) {
         while(nums.size()>1){
             vector<int> v;
-            // cout<<endl<<"curr num size "<<nums.size()<<endl;
             for(int i=1; i<nums.size(); i++){
                 int curr = nums[i-1]; 
                 int next = nums[i];
                 int sum = curr + next;
                 // ok we need only ones digit 
                 sum=sum%10;
-                // cout<<sum<<" ";
                 v.push_back(sum);
             }
             nums=v;
