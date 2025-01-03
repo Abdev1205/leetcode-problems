5d48e11396c8ea3905e1a192e4115650557b8996
Abhay Mishra <abhaym1205@gmail.com>
2025-01-03 20:06:17 +0530

    Time: 0 ms (100.00%) | Memory: 89.4 MB (62.32%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -4,12 +4,11 @@ public:
         long long index = 0;
         long long lSum = nums[index];
         long long rSum = accumulate(nums.begin(),nums.end(), 0LL);
-        // cout<<rSum<<endl;
+
         long long ans = 0;
         rSum-=lSum;
         long long len = nums.size();
         while(index<len-1){
-            // cout<<lSum<<" : "<<rSum<<" = "<<index<<endl;
             if(lSum>=rSum){
                 ans++;
             }
