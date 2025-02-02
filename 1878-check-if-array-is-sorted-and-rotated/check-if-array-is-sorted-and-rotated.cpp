cc21059a671076a1db4e96336a8cc27c4376711a
Abhay Mishra <abhaym1205@gmail.com>
2025-02-02 23:55:44 +0530

    Time: 15 ms (0.60%) | Memory: 12.2 MB (5.21%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,17 +1,57 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
-        int counter=0;
-        for(int i=1; i<n; i++){
-            if(nums[i-1]>nums[i]){
-                counter++;
+        // brute force 
+        // I know this approach is not efficient at all 
+        // I want to follow atmost basic approach
+        int len = nums.size();
+        // 1st I will find the peak element
+        int peak = 0;
+        for (int i = 1; i < len; i++) {
+            int curr = nums[i - 1];
+            int next = nums[i];
+            if (next < curr) {
+                peak = i;
+                break;
             }
         }
-        if(nums[n-1]>nums[0]){
-            counter++;
-        }
-        return counter<=1;
 
+        cout << "Peak --> " << peak << endl;
+
+        // Now I can divide single array in 2 array and then combine to form rotated
+        // sorted If sorted so return true else false
+
+        vector<int> left(nums.begin(), nums.begin() + peak);
+        vector<int> right(nums.begin() + peak, nums.end());
+
+        cout << "Printing Left --> " << endl;
+        for (auto a : left) {
+            cout << a << " ";
+        }
+        cout << endl;
+        cout << "Printing Right --> " << endl;
+
+        for (auto a : right) {
+            cout << a << " ";
+        }
+        vector<int> rotArr;
+        if (left.empty() || right.empty() || left[0] < right[0]) {
+            rotArr.insert(rotArr.end(), left.begin(), left.end());
+            rotArr.insert(rotArr.end(), right.begin(), right.end());
+        } else {
+            rotArr.insert(rotArr.end(), right.begin(), right.end());
+            rotArr.insert(rotArr.end(), left.begin(), left.end());
+        }
+
+        cout<<endl << "Rotated Array --> " << endl;
+        for (auto a : rotArr) {
+            cout << a << " ";
+        }
+
+        if(is_sorted(rotArr.begin(),rotArr.end())){
+            return true;
+        }
+
+        return false;
     }
 };
