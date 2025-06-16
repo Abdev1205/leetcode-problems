796b95b8396a85f3e91b1840cb5ff3354a0a5190
Abhay Mishra <abhaym1205@gmail.com>
2025-06-16 23:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (95.44%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
      // trying inplace 
-     // if we will update all the value inTime so it may give wrong answer
+     // if we will update all the value in sam time so it may give wrong answer
      // So I will create a vector of pair 
      // we will be storing thr row and col were we have to do opeartion
-     // instead of thinking about the data consitency
 
      vector<pair<int,int>> op;
      // getting all the operation from the array
@@ -13,17 +12,13 @@ public:
         for(int col=0; col<matrix[row].size(); col++){
             // if it is zero then store in vector
             if(matrix[row][col]==0){
-                pair<int,int> p;
-                p.first = row;
-                p.second = col;
-                op.push_back(p);
+                op.push_back({row,col});
             }
         }
      }
      for(auto o:op){
         int row = o.first;
         int col = o.second;
-        cout<<row<<" : "<<col<<endl;
 
         // row operation
         for(int i=0; i<matrix[row].size(); i++){
