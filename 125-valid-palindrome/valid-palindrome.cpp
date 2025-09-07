c6abf2aa2ed79a290e604386d4541b939d2b4543
Abhay Mishra <abhaym1205@gmail.com>
2025-09-07 20:28:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (58.13%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(string str) {
-        // Two Pointer Approach 
-        int len = str.size();
-        int s=0;
-        int e=len-1;
+        int s= 0; 
+        int e= str.size();
 
         while(s<=e){
-            if(!isalnum(str[s])){
-                s++;
-                continue;
-            }
-            if(!isalnum(str[e])){
-                e--;
-                continue;
-            }
-
             if(tolower(str[s])==tolower(str[e])){
                 s++;
                 e--;
                 continue;
             }
+            else if(!isalnum(str[s])){
+                s++;
+                continue;
+            }
+            else if(!isalnum(str[e])){
+                e--;
+                continue;
+            }
             return false;
-
         }
+
         return true;
     }
 };
