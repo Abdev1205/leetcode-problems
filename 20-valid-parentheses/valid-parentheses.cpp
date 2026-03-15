b0c78918b6bfe982dafb307232931f9ff8769e22
Abhay Mishra <abhaym1205@gmail.com>
2026-03-15 20:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.85%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int> st;
+        stack<char> bucket;
 
         for(int i=0; i<s.size(); i++){
             char ch = s[i];
-            // if opening braces 
-            if(ch=='(' || ch=='[' || ch=='{'){
-                st.push(ch);
+
+            if( (ch=='(') || (ch=='{') || (ch=='[') ){
+                // add that in bucket
+                bucket.push(ch);
             }
             else{
-                // if closing braces are there 
-                if(!st.empty()){
-                    char topCh = st.top();
-                    if(ch==')' && topCh =='(' ){
-                        st.pop();
+               if(!bucket.empty()){
+                    // this is closing bracket so we have to remove its 
+                    // corresponding operning bracket
+                    char top = bucket.top();
+                    if(ch==')' && top=='('){
+                        // its match remove that opening bracket
+                        bucket.pop();
                     }
-                    else if(ch==']' && topCh =='['){
-                        st.pop();
+                    else if(ch=='}' && top=='{'){
+                        // its match remove that opening bracket
+                        bucket.pop();
                     }
-                    else if(ch=='}' && topCh=='{'){
-                        st.pop();
+                    else if(ch==']' && top=='['){
+                        // its match remove that opening bracket
+                        bucket.pop();
                     }
-                    else {
+                    else{
                         return false;
                     }
-                    
-                }
-                else {
+               }
+               else{
                     return false;
-                }
+               }
             }
+        }
 
-        }
-        
-        if(st.empty()){
-            return true;
-        }
-        else{
-           return false; 
-        } 
+        return bucket.size()==0;
     }
 };
