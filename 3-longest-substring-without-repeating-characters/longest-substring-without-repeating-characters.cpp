6be42479972c88c4deaac3d9b0a4ab6b66b1d8d8
Abhay Mishra <abhaym1205@gmail.com>
2026-03-15 19:05:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (85.71%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l =0;
-        int r =0;
-        int maxlen = 0;
-        vector<int> freq(256,-1);
+        int l=0;
+        int r=0;
+        int maxlen =0;
+
         int n = s.size();
 
+        vector<int> hash(256,-1);
+
         while(r<n){
-            // if we found the r in freq so we just update the l 
-            if(freq[s[r]]!=-1){
-                // if r is already present in freq and also it is in range
-                if(freq[s[r]]>=l){
-                    // updating the l so that to avoid repeating char
-                    l = freq[s[r]]+1;
+            if(hash[s[r]]!=-1){
+                // which means alrady there so we have to update the l and hash
+                if(hash[s[r]]>=l){
+                    l = hash[s[r]] + 1;
                 }
             }
+
             int len = r-l+1;
-            maxlen = max(maxlen,len);
-            freq[s[r]]=r;
+            maxlen = max(len,maxlen);
+            hash[s[r]] = r;
             r++;
         }
+
         return maxlen;
+
     }
 };
