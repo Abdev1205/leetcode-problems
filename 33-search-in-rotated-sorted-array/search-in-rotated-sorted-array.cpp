1b9b8ebc9e5f3583161a2d1b55327e95bec9eb08
Abhay Mishra <abhaym1205@gmail.com>
2026-03-15 22:47:42 +0530

    Time: 4 ms (0.87%) | Memory: 15.2 MB (66.97%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,57 +1,58 @@
 class Solution {
 public:
-    int findPeak(vector<int>& nums) {
+    int findPeak(vector<int>& nums){
         int start = 0;
-        int end = nums.size() - 1;
+        int end = nums.size()-1;
 
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > nums[end]) {
-                start = mid + 1;
-            } else {
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]>nums[end]){
+                start = mid+1;
+            }
+            else{
                 end = mid;
             }
         }
+
         return start;
     }
 
-    int findBinary(int start, int end, int target, vector<int>& nums) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
+    int binary(vector<int>& nums, int target, int start, int end){
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
                 return mid;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+            }
+            else if(target>nums[mid]){
+                start = mid +1;
+            }
+            else {
+                end = mid-1;
             }
         }
+
         return -1;
     }
 
+
     int search(vector<int>& nums, int target) {
-        // Approach 1
-        // First we can find the peak/pivot index 
-        // and then we can divide the array in two parts 
-        // then we can apply the binary search in each part
-        // Finding the pivot element
         int pivot = findPeak(nums);
+        cout<<"Pivot -> "<<pivot<<endl;
+
+        // break array in two sorted array and do binary search ther e
         
-        // If we found the target at pivot, return pivot
-        if (nums[pivot] == target) {
-            return pivot;
+        int firstPart = binary(nums,target, 0, pivot-1);
+        if(firstPart!=-1){
+            return firstPart;
         }
 
-        // Perform binary search in both parts
-        int ans1 = findBinary(0, pivot - 1, target, nums);
-        if (ans1 != -1) {
-            return ans1;
+        int secondPart = binary(nums,target, pivot,nums.size()-1);
+        if(secondPart!=-1){
+            return secondPart;
         }
 
-        int ans2 = findBinary(pivot, nums.size() - 1, target, nums);
-        if (ans2 != -1) {
-            return ans2;
-        }
+        cout<<"Returning last"<<endl;
 
         return -1;
     }
